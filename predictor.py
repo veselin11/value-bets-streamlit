b28201d47672ddd0babf8713d6ccdbeaed6c91d5
VAT <tyurdievv@gmail.com>
2025-05-17 14:28:11 +0300

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -8,23 +8,28 @@ def predict(matches_df):
 
     df = matches_df.copy()
 
-    # Функция за добавяне на нови стойности в енкодера
-    def extend_encoder(encoder, values):
-        known = set(encoder.classes_)
-        unknown = set(values) - known
-        if unknown:
-            encoder.classes_ = np.array(list(encoder.classes_) + list(unknown))
-        return encoder
+    def encode_with_unseen(encoder, values):
+        classes = set(encoder.classes_)
+        # Ако стойностите не са в класовете, заместете с -1
+        encoded = []
+        for v in values:
+            if v in classes:
+                encoded.append(encoder.transform([v])[0])
+            else:
+                encoded.append(-1)  # или някаква фиктивна стойност
+        return np.array(encoded)
 
-    enc_team1 = extend_encoder(encoders["team1"], df["Отбор 1"])
-    enc_team2 = extend_encoder(encoders["team2"], df["Отбор 2"])
-    enc_league = extend_encoder(encoders["league"], df["Лига"])
-
-    df["Отбор 1"] = enc_team1.transform(df["Отбор 1"])
-    df["Отбор 2"] = enc_team2.transform(df["Отбор 2"])
-    df["Лига"] = enc_league.transform(df["Лига"])
+    df["Отбор 1"] = encode_with_unseen(encoders["team1"], df["Отбор 1"])
+    df["Отбор 2"] = encode_with_unseen(encoders["team2"], df["Отбор 2"])
+    df["Лига"] = encode_with_unseen(encoders["league"], df["Лига"])
 
     X = df[["Отбор 1", "Отбор 2", "Лига", "Коеф"]]
+
+    # Ако моделът не може да работи с -1 стойности (тъй като RandomForest може), 
+    # можеш да ги замениш с най-често срещаната стойност или 0.
+    # Пример:
+    X = X.replace(-1, 0)
+
     preds = model.predict_proba(X)[:, 1]
 
     results = matches_df.copy()
