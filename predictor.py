03322d8c642d3f89410c4c00cb4a1e831e2452ac
VAT <tyurdievv@gmail.com>
2025-05-17 14:40:56 +0300

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -8,29 +8,25 @@ def predict(matches_df):
 
     df = matches_df.copy()
 
-    def encode_with_unseen(encoder, values):
-        classes = set(encoder.classes_)
-        encoded = []
-        for v in values:
-            if v in classes:
-                encoded.append(encoder.transform([v])[0])
-            else:
-                # unseen label -> encode as -1
-                encoded.append(-1)
-        return np.array(encoded)
+    # Функция за добавяне на нови стойности в енкодера
+    def extend_encoder(encoder, values):
+        known = set(encoder.classes_)
+        unknown = set(values) - known
+        if unknown:
+            encoder.classes_ = np.array(list(encoder.classes_) + list(unknown))
+        return encoder
 
-    df["Отбор 1"] = encode_with_unseen(encoders["team1"], df["Отбор 1"])
-    df["Отбор 2"] = encode_with_unseen(encoders["team2"], df["Отбор 2"])
-    df["Лига"] = encode_with_unseen(encoders["league"], df["Лига"])
+    enc_team1 = extend_encoder(encoders["team1"], df["Отбор 1"])
+    enc_team2 = extend_encoder(encoders["team2"], df["Отбор 2"])
+    enc_league = extend_encoder(encoders["league"], df["Лига"])
+
+    df["Отбор 1"] = enc_team1.transform(df["Отбор 1"])
+    df["Отбор 2"] = enc_team2.transform(df["Отбор 2"])
+    df["Лига"] = enc_league.transform(df["Лига"])
 
     X = df[["Отбор 1", "Отбор 2", "Лига", "Коеф"]]
-
-    # Заменяме -1 с 0 (най-често срещана категория)
-    X = X.replace(-1, 0)
-
     preds = model.predict_proba(X)[:, 1]
 
     results = matches_df.copy()
     results["value"] = preds
-
     return results[results["value"] > 0.5].sort_values(by="value", ascending=False)
