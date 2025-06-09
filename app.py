8d924ff3704c44acb9a1730216fb9bf81ac0eb92
VAT <tyurdievv@gmail.com>
2025-06-09 12:07:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import sqlite3
 import pandas as pd from datetime
-import datetime
+import pandas as pd
+from datetime import datetime
 
 ------------------- DB -------------------
 
